92b77ad1526488510466d1ccbb8a6d61cb1e38de
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2022-06-05 17:43:52 -0500

    First pasing test for tree normalizing

--- src/CoreTests/FsCheck.FsSpec.fs
+++ src/CoreTests/FsCheck.FsSpec.fs
@@ -1,44 +1,16 @@
 ﻿module GeneratorExperiment
+open FsSpec.CustomTree
 
-module Gen = 
-    open FsSpec.CustomTree
-    open FsCheck
-
-    let postwalk (constraints:Constraint<'a>) = 
-        ()
-
+module Constraint = 
     let normalizeToDistributedAnd (constraints:Constraint<'a>) = 
-        // Really I want a post-order tree walk. This is where a generic tree implementation would be very helpful
-        // does cata satisfy this?
-        // Hmm, maybe it doesn't need to be post-order. I think I can do this just by recursive fold (or iterate until 1-layer deep or remains)
-        let isLeaf = function | ConstraintLeaf _ -> true | _ -> false
-        let distributeSingle andList = 
-            let (nonCombinators, combinators) = andList |> List.partition isLeaf
-            let distributeToCombinator toDistribute childConstr = 
-                // something is fishy here. I expect all ands to end up in one layer topped by their nearest or
-                // here I end up accounting for non-combinators twice, but I also need to account for the case there are no combinators at all
-                // should I be able to end up with only one top-level or? 
-                match childConstr with
-                | Combinator (And, l) -> Combinator (And, Seq.concat [l; toDistribute])
-                | Combinator (Or, l) -> Combinator (Or, l |> Seq.map (fun c -> Constraint.Factories.all (c :: (List.ofSeq toDistribute))))
-            
-            if List.isEmpty combinators then
-                constraints
-            else 
-                // what do I really want this to look like? I 
-                combinators 
-                |> Seq.map distributeToCombinator nonCombinators
-                |> Combinator
-                    
-            Constraint.Factories.any (andList |> Seq.map distributeToChild nonCombinators)
+        Constraint.Factories.all [constraints]
 
-        match constraints with
-        | Constraint.Combinator (And, children) -> distributeSingle children
-        | _ -> constraints
-
+//module Gen = 
+//    open FsCheck
 
     //let fromConstraint (constraints:Constraint<'a>) : Gen<'a> =
     //   Gen.
     //   Arb.generate<'a> |> 
     //   Arb.
+
 
---
+++ src/CoreTests/TreeNormalizationTests.fs
@@ -0,0 +1,31 @@
+﻿module TreeNormalizationTests
+
+open Expecto
+open FsSpec.CustomTree
+open GeneratorExperiment
+open FsCheck
+open System
+
+type CustomArb =
+    static member IComparableInt() = 
+        Arb.generate<int>
+        |> Gen.map (fun i -> i :> IComparable<int>)
+        |> Arb.fromGen
+
+    static member Regex() =
+        Arb.generate<Guid>
+        |> Gen.map (string >> System.Text.RegularExpressions.Regex)
+        |> Arb.fromGen
+
+
+let testProperty' = testPropertyWithConfig { FsCheckConfig.defaultConfig with arbitrary = [typeof<CustomArb>] }
+
+[<Tests>]
+let tests = testList "Constraint Tree Normalization" [
+    testProperty' "Top layer is always AND" <| fun (tree: Constraint<int>) ->
+        let normalized = Constraint.normalizeToDistributedAnd tree
+         
+        match normalized with
+        | Combinator (And, _) -> true
+        | _ -> false
+]
