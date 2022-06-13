e33cae2778ecceb3c42c2a278ba98fba760f4af9
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2022-06-13 09:57:38 -0500

    Allow differentiated access between constraint union cases and case types

--- src/FsSpec.Core/ConstraintModel.fs
+++ src/FsSpec.Core/ConstraintModel.fs
@@ -2,17 +2,24 @@
 
 open System
 
-// hmm. I think these being separate isn't great for discoverability. It also means 
-type ConstraintLeaf<'a> = 
-    | None
-    | Max of IComparable<'a> 
-    | Min of IComparable<'a>
-    | Regex of System.Text.RegularExpressions.Regex
-    // probably want to include some kind of "meta" field so that custom types can do things like make specific contraint-definition time values available to formatters
-    // for example: customMax 20 would be ("customMax", {max: 20}, (fn value -> value <= 20)) with formatter | Custom ("customMax", meta, _) -> $"max {meta.max}" 
-    | Custom of (string * ('a -> bool))
+[<AutoOpen>]
+module Internal =
+    type ConstraintLeaf<'a> = 
+        | None
+        | Max of IComparable<'a> 
+        | Min of IComparable<'a>
+        | Regex of System.Text.RegularExpressions.Regex
+        // probably want to include some kind of "meta" field so that custom types can do things like make specific contraint-definition time values available to formatters
+        // for example: customMax 20 would be ("customMax", {max: 20}, (fn value -> value <= 20)) with formatter | Custom ("customMax", meta, _) -> $"max {meta.max}" 
+        | Custom of (string * ('a -> bool))
 
-type Combinator<'a> = | And | Or
+    module ConstraintLeaf = 
+        let isMax = (function | Max _ -> true | _ -> false)
+        let isMin = (function | Min _ -> true | _ -> false)
+        let isRegex = (function | Regex _ -> true | _ -> false)
+        let isNone = (function | None -> true | _ -> false)
+
+    type Combinator<'a> = | And | Or
 
 type Constraint<'a> =
     | ConstraintLeaf of ConstraintLeaf<'a>
