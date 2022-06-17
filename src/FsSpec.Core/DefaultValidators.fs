d354172433cad401b35e0ea14b7d35c02a2bb807
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2022-06-17 16:11:46 -0500

    remove now unused combinator validators

--- src/FsSpec.Core/DefaultValidators.fs
+++ src/FsSpec.Core/DefaultValidators.fs
@@ -14,28 +14,3 @@ module DefaultValidators =
         | null -> false
         | :? System.String as str -> regex.IsMatch(str) 
         | _ -> invalidArg (nameof value) $"Regex can only validate strings not {value.GetType().FullName}"
-
-
-    // these are really just the "and" and "or" operations for a result type. Would probably be better to create parameterized versions
-    // then build up my specific case
-    let validateAnd value childResults = 
-        let combine left right =
-            match (left, right) with 
-            | Ok _, Ok _ -> left
-            | Ok _, Error err -> Error err
-            | Error err, Ok _ -> Error err
-            | Error errLeft, Error errRight -> Error (List.concat [errLeft; errRight])
-        match childResults with
-        | [] -> Ok value
-        | _ -> childResults |> List.reduce combine
-
-    let validateOr value childResults = 
-        let combine left right =
-            match (left, right) with 
-            | Ok _, Ok _ -> left
-            | Ok ok, Error _ -> Ok ok
-            | Error err, Ok ok -> Ok ok
-            | Error errLeft, Error errRight -> Error (List.concat [errLeft; errRight])
-        match childResults with
-        | [] -> Ok value
-        | _ -> childResults |> List.reduce combine
