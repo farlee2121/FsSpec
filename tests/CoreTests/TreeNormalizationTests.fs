14b3abf3aebbfd6e37b5c17a2942062f0f8bafe0
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2024-02-06 11:27:20 -0600

    Update sdk to net8

--- tests/CoreTests/TreeNormalizationTests.fs
+++ tests/CoreTests/TreeNormalizationTests.fs
@@ -9,7 +9,7 @@ open Force.DeepCloner
 open FsSpec.Normalization
 
 
-let testProperty' name test = 
+let testProperty name test = 
     testPropertyWithConfig { FsCheckConfig.defaultConfig with arbitrary = [typeof<DefaultSpecArbs>] } name test
 
 module Array =
@@ -20,17 +20,17 @@ module Array =
 
 [<Tests>]
 let depthTests = testList "Tree depths tests" [
-    testProperty' "Depth always more than zero" <| fun (spec: Spec<int>) ->
+    testProperty "Depth always more than zero" <| fun (spec: Spec<int>) ->
         Spec.depth spec >! 0
-    testProperty' "Leaf always depth 1" <| fun (leaf: SpecLeaf<int>) ->
+    testProperty "Leaf always depth 1" <| fun (leaf: SpecLeaf<int>) ->
         (Spec.depth (SpecLeaf leaf)) =! 1
-    testProperty' "Any combinator without children has depth 1" <| fun (comb: Combinator<int>) ->
+    testProperty "Any combinator without children has depth 1" <| fun (comb: Combinator<int>) ->
         Spec.depth (Combinator (comb, [])) =! 1
-    testProperty' "Any combinator with children has depth at least 2" 
+    testProperty "Any combinator with children has depth at least 2" 
     <| fun (comb: Combinator<int>, children: NonEmptyArray<Spec<int>>) ->
         let children = List.ofArray children.Get
         Spec.depth (Combinator (comb, children)) >=! 2
-    testProperty' "N nested combinators always N deep" <| fun (combinators: NonEmptyArray<Combinator<int>>) ->
+    testProperty "N nested combinators always N deep" <| fun (combinators: NonEmptyArray<Combinator<int>>) ->
         let head,tail = combinators.Get |> Array.splitHead
         let spec = tail
                    |> List.fold (fun agg c -> Spec.Combinator (c,[agg])) (Combinator (head, []))
@@ -65,28 +65,28 @@ let treeEqual left right : bool =
 
 [<Tests>]
 let treeEquality = testList "Tree equal" [
-    testProperty' "Any tree equals itself" <| fun (spec: Spec<int>) ->
+    testProperty "Any tree equals itself" <| fun (spec: Spec<int>) ->
         treeEqual spec spec
 
-    testProperty' "Equality is structural, not referential" <| fun (spec: Spec<int>) ->
+    testProperty "Equality is structural, not referential" <| fun (spec: Spec<int>) ->
         treeEqual spec (spec.DeepClone())
         
 ]
 
 [<Tests>]
 let trimTests = testList "Trim Empty Branches" [
-    testProperty' "Leaf trims to itself" <| fun (spec: CustomGenerators.LeafOnly<int>) ->
+    testProperty "Leaf trims to itself" <| fun (spec: CustomGenerators.LeafOnly<int>) ->
         let spec = spec.Spec
         test <@ treeEqual (Spec.trimEmptyBranches spec) spec @>
 
-    testProperty' "Leafless trees return None" <| fun (tree: CustomGenerators.LeaflessSpecTree<int>) ->
+    testProperty "Leafless trees return None" <| fun (tree: CustomGenerators.LeaflessSpecTree<int>) ->
         test <@ treeEqual (Spec.trimEmptyBranches tree.Spec) (SpecLeaf SpecLeaf.None) @>
 
-    testProperty' "Combinator with only leafs is unchanged" <| fun (root:Combinator<int>, leafs:NonEmptyArray<SpecLeaf<int>>) ->
+    testProperty "Combinator with only leafs is unchanged" <| fun (root:Combinator<int>, leafs:NonEmptyArray<SpecLeaf<int>>) ->
         let tree = (Combinator (root, leafs.Get |> List.ofArray |> List.map SpecLeaf))
         test <@ treeEqual (Spec.trimEmptyBranches tree) tree @>
 
-    testProperty' "Combinator with mixed leaves and empty combinators returns only Leafs" 
+    testProperty "Combinator with mixed leaves and empty combinators returns only Leafs" 
     <| fun (root:Combinator<int>, leafs:NonEmptyArray<SpecLeaf<int>>, emptyBranches: NonEmptyArray<Combinator<int>>) ->
         let expectedChildren = leafs.Get |> List.ofArray |> List.map SpecLeaf
         let emptyBranch op = Combinator (op, [])
@@ -100,7 +100,7 @@ let trimTests = testList "Trim Empty Branches" [
         |> Spec.getChildren
         |> List.forall2 treeEqual expectedChildren
 
-    testProperty'
+    testProperty
         "Combinator with non-empty combinators remains unchanged" <| fun (tree: NoEmptyBranches<int>)  ->
             let tree = tree.Spec
             test <@ treeEqual (Spec.trimEmptyBranches tree) tree @>
@@ -109,14 +109,14 @@ let trimTests = testList "Trim Empty Branches" [
 
 [<Tests>]
 let tests = testList "Spec Tree Normalization" [
-    testProperty' "Top layer is always OR" <| fun (tree: Spec<int>) ->
+    testProperty "Top layer is always OR" <| fun (tree: Spec<int>) ->
         let normalized = Spec.normalizeToDistributedAnd tree
          
         match normalized with
         | Combinator (Or, _) -> true
         | _ -> false
 
-    testProperty' "Second layer is always AND" <| fun (tree: Spec<int>) ->
+    testProperty "Second layer is always AND" <| fun (tree: Spec<int>) ->
         let normalized = Spec.normalizeToDistributedAnd tree
          
         let isAnd = (function | (Combinator (And,_)) -> true | _ -> false)
@@ -124,28 +124,28 @@ let tests = testList "Spec Tree Normalization" [
         | Combinator (Or, children) -> children |> List.forall isAnd
         | _ -> false
 
-    testProperty' "AND groups contain no combinators (spec tree is 3 deep)" <| fun (spec: GuaranteedLeafs<int>) ->
+    testProperty "AND groups contain no combinators (spec tree is 3 deep)" <| fun (spec: GuaranteedLeafs<int>) ->
         let normalized = Spec.normalizeToDistributedAnd spec.Spec
         test <@ Spec.depth normalized = 3 @>
 
-    testProperty' "Any spec without leaves (combinators-only) normalizes to a single form" <| fun (spec: LeaflessSpecTree<int>) ->
+    testProperty "Any spec without leaves (combinators-only) normalizes to a single form" <| fun (spec: LeaflessSpecTree<int>) ->
         let normalized = Spec.normalizeToDistributedAnd spec.Spec
         match normalized with 
         | (Combinator (Or, [Combinator (And, [SpecLeaf SpecLeaf.None])])) -> ()
         | other -> failtest $"Expected default empty spec, got {other}"
 
-    testProperty' "Original and normalized expressions are logically equivalent" <| fun (spec: OnlyLeafsForType<int>) ->
+    testProperty "Original and normalized expressions are logically equivalent" <| fun (spec: OnlyLeafsForType<int>) ->
         let spec = spec.Spec
         let normalized = Spec.normalizeToDistributedAnd spec
         Check.QuickThrowOnFailure <| fun (i:int) ->
             test <@ Spec.isValid normalized i = Spec.isValid spec i @>
 
 
-    testProperty' "Normalization is idempotent" <| fun (spec: Spec<int>) ->
+    testProperty "Normalization is idempotent" <| fun (spec: Spec<int>) ->
         let normalized = Spec.normalizeToDistributedAnd spec
         treeEqual normalized (Spec.normalizeToDistributedAnd normalized)
 
-    testProperty' "Any normal-form spec remains unchanged" <| fun (leafGroups: NonEmptyArray<NonEmptyArray<SpecLeaf<int>>>) ->
+    testProperty "Any normal-form spec remains unchanged" <| fun (leafGroups: NonEmptyArray<NonEmptyArray<SpecLeaf<int>>>) ->
         // Subtly different than idempotence. Idempotence can be achieved by always returning the same value from the normalizer
         let normalTree = 
             leafGroups.Get |> List.ofArray
